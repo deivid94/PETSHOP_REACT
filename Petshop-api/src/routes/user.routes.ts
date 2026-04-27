ece0acf2dfe0626a0fc80026e36daf4e9b4338f9
deivid94 <md031194@gmail.com>
2026-04-26 22:33:30 -0300

    feature wt: create post, controller service for created new user

--- Petshop-api/src/routes/user.routes.ts
+++ Petshop-api/src/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import getAll from "../controllers/getAll.js"
+import { createNewUser } from "../controllers/CreateNewUSer.js"
 
 const userRouter =  Router()
 userRouter.route("/cliente")
 
 .get(getAll)
+.post(createNewUser)
 export default userRouter
--- Petshop-api/src/server.ts
+++ Petshop-api/src/server.ts
@@ -4,6 +4,7 @@ import "dotenv/config"
 
 const app = express()
 app.use (express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(userRouter,)
 
 
